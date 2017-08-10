4c24697f632d7e4bcb4623c3630f45f5c4d41895
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2017-08-10 15:59:49 -0700

    Add guard and first test

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 
-gem 'rspec', '~> 3.0'
+group :development do
+  gem 'rspec', '~> 3.0'
+  gem 'guard-rspec', require: false
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/board'
+
+RSpec.describe Board, '#draw' do
+  context 'When draw is called' do
+    it 'draws the board' do
+      board = Board.new
+    end
+  end
+end
