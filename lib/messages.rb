b91cc94767c26cca9c79e4fb59fd9595b5089e8f
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2017-10-20 13:50:32 -0700

    Removes extra string assignment from messages

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,10 +1,7 @@
 class Messages
 
   def pre_render_welcome
-    string = ""
-    string << "Let's play tic tac toe"
-
-    string
+    "Let's play tic tac toe"
   end
 
   def pre_render_game_type
@@ -18,59 +15,39 @@ class Messages
   end
 
   def pre_render_select_game_type
-    string = ""
-    string << "Please select 1, 2 or 3."
-
-    string
+    "Please select 1, 2 or 3."
   end
 
   def pre_render_prompt_player_for_move
-    string = ""
-    string << "Please make your move"
-
-    string
+    "Please make your move"
   end
 
   def pre_render_prompt_select_space
-    string = ""
-    string << "Please select a valid space on the board."
-
-    string
+    "Please select a valid space on the board."
   end
 
   def pre_render_cats_game
-    string = ""
-    string << "Cat's game!!"
-
-    string
+    "Cat's game!!"
   end
 
   def pre_render_computer_thinking
-    string = ""
-    string << "The computer is thinking..."
+    "The computer is thinking..."
   end
 
   def pre_render_player_turn(game)
-    string = ""
-    string = "Its time for #{game.current_player.symbol} to go"
+    "Its time for #{game.current_player.symbol} to go"
   end
 
   def pre_render_player_choice(game)
-    string = ""
-    string = "#{game.current_player.symbol} has chosen"
+    "#{game.current_player.symbol} has chosen"
   end
 
   def pre_render_player_move(ui)
-    string = ""
-    string = "Your move is #{ui.move}"
+    "Your move is #{ui.move}"
   end
 
   def pre_render_winner(board, game, board_evaluator)
-    string = ""
-    string << game.winner(board, board_evaluator)
-    string << " has won the game!"
-
-    string
+    "#{game.winner(board, board_evaluator)} has won the game!"
   end
 
 end
