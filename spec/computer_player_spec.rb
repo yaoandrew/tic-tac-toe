6ba57bd382e060b8f79ffd1a096c1bc7bf0224b0
yaoandrew <1547809+yaoandrew@users.noreply.github.com>
2017-10-20 11:00:21 -0700

    Add tests for computer player

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,17 +1,63 @@
 require_relative '../lib/computer_player'
 require_relative '../lib/board'
+require_relative '../lib/board_evaluator'
+require_relative '../lib/user_interface'
+require_relative '../lib/game'
+require_relative '../lib/validator'
 
-describe ComputerPlayer, '#make_simple_move' do
-  context 'When the computer is asked' do
-    it 'makes a simple move' do
+describe ComputerPlayer, '#make_smart_move' do
+
+  context 'When the board has a nearly winning move by the opposing player' do
+    it 'Makes a smart move that blocks the win' do
       symbol = 'O'
       computer_player = ComputerPlayer.new(symbol)
       board = Board.new
-      board.cells = %w( X X X
+      board_evaluator = BoardEvaluator.new
+      board.turn = Board::PLAYER_O
+      board.cells = %w( X X -
                         - - -
                         - - - )
-      result = computer_player.make_simple_move(board)
-      expect(result).to be > 2
+      result = computer_player.make_smart_move(board, board_evaluator)
+      expect(result).to eq 2
     end
   end
+
+  context 'When the board has a nearly winning move' do
+    it 'Makes a smart move, that will win the game' do
+      symbol = 'O'
+      computer_player = ComputerPlayer.new(symbol)
+      board = Board.new
+      board_evaluator = BoardEvaluator.new
+      board.turn = Board::PLAYER_O
+      board.cells = %w( O X -
+                        - O -
+                        - - - )
+      result = computer_player.make_smart_move(board, board_evaluator)
+      expect(result).to eq 8
+    end
+  end
+
+end
+
+describe ComputerPlayer, '#play_game' do
+
+  context 'When called with a game, ui, board, and board_evaluator' do
+    it 'Returns a board with a move made' do
+      player1 = ComputerPlayer.new('X')
+      player2 = ComputerPlayer.new('O')
+      validator = Validator.new
+      game = Game.new(player1, player2)
+      ui = UserInterface.new(validator)
+      board = Board.new
+      board_evaluator = BoardEvaluator.new
+      board.turn = Board::PLAYER_O
+      board.cells = %w( X O -
+                        - - -
+                        - - - )
+      result = player2.play_game(game, ui, board, board_evaluator)
+      expect(result.is_a?(Board)).to be true
+
+    end
+  end
+
 end
